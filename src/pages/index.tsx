dffae70bc6ef71d16ff8eb5e91bfdada41af7011
no-ikjun <choiikjun1101@gmail.com>
2023-04-29 13:53:22 +0900

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,7 @@ export default function Home() {
 
           <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
             <h2>
-              Deploy <span>-&gt;</span>
+              배포 <span>-&gt;</span>
             </h2>
             <p>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.</p>
           </a>
