28a9bde7220d3943bd76f0a13b1b7100d936cd44
no-ikjun <choiikjun1101@gmail.com>
2023-04-30 00:03:47 +0900

    join group page

--- src/pages/join_group.tsx
+++ src/pages/join_group.tsx
@@ -1,3 +1,27 @@
+import styles from "../styles/JoinGroup.module.css";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
 export default function JoinGroup() {
-  return <></>;
+  const [windowSize, setWindowSize] = useState(0);
+  useEffect(() => {
+    setWindowSize(window.innerHeight);
+  }, []);
+  return (
+    <div className={styles.group_div}>
+      <div className={[styles.container, "group-div"].join(" ")}>
+        <h1 className={styles.title}>그룹 가입하기</h1>
+        <div className={styles.join_div}>
+          <div className={styles.join_input_div}>
+            <input type="text" placeholder="그룹 코드를 입력하세요!" className={styles.join_input} />
+          </div>
+          <button className={styles.group_join_btn}>가입하기</button>
+          <p className={styles.group_create_ment}>혹은</p>
+          <Link href="/create_group" className={styles.group_create_btn}>
+            새로운 그룹 생성하기 →
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
