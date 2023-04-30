a558f607af9414137e0bc77d3f2e0e8648832bf5
no-ikjun <choiikjun1101@gmail.com>
2023-04-30 15:07:08 +0900

    bug fix 2

--- src/pages/answer/index.tsx
+++ src/pages/answer/index.tsx
@@ -110,28 +110,6 @@ export default function Answer() {
               );
             }
           })}
-          <div className={styles.answer_list}>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                <span className={styles.answer_list_date}>29일</span>
-                <p
-                  className={styles.answer_list_question}
-                  onClick={() => {
-                    answer !== "1" ? setAnswer("1") : setAnswer("");
-                  }}
-                >
-                  Q. 오늘 먹은 음식은 무엇인가요?
-                </p>
-              </div>
-              <p className={styles.answer_list_answer} style={{ display: `${answer === "1" ? "block" : "none"}` }}>
-                A. 오늘은 이것과 저것을 먹었습니다. 정말 맛있었습니다.
-              </p>
-            </div>
-            <Link href="/answer/public?question=af69319d-c601-4147-bf30-19edea518378" className={styles.answer_list_range}>
-              다른 의견 보기 →
-            </Link>
-          </div>
-          <hr className={styles.answer_list_hr} />
         </div>
       </div>
     </div>
