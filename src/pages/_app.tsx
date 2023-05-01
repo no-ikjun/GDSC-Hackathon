a2c94fb9bb73fe41b4b77609e68e494658d33218
no-ikjun <choiikjun1101@gmail.com>
2023-05-01 10:01:24 +0900

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "next-themes";
 import { CookiesProvider } from "react-cookie";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CookiesProvider>
       <Layout>
+        <Analytics />
         <Component {...pageProps} />
       </Layout>
     </CookiesProvider>
